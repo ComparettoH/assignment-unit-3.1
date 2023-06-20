b6ed7a8703041a8aeec10c155d70efc6cfbc6143
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-06-20 13:26:04 -0500

    1a and 1b answers

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,11 +8,11 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-
+let favoriteFoods=['tacos', 'pasta', 'pizza', 'sushi'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
-
+console.log('Hannahs favorite foods are:', favoriteFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
