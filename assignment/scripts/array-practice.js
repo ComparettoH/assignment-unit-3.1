aa6d287d053d8b95a7b67c576478e2de858dc7ff
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-05 21:03:19 -0500

    work on stretch goals/todos

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -85,16 +85,28 @@ console.log('The favorite foods are now', favoriteFoods);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+favoriteFoods[1] = 'carbs';
+console.log('updated favorite foods are:', favoriteFoods)
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+favoriteFoods.sort();
+console.log('Favorite foods alphabetical order,', favoriteFoods)
+favoriteFoods.reverse();
+console.log('Favorite foods order is now,', favoriteFoods)
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+favoriteFoods.toString()
+let text = favoriteFoods.toString();
+console.log('favorite foods are:', text)
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+
+let newArray = favoriteFoods + animalArray;
+console.log('The newArray created is:', newArray)
